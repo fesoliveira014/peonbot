781e5445ee0d6f4a19357e4f831b05987bbedb54
fesoliveira014 <fesoliveira@outlook.com>
2015-09-13 01:22:22 -0300

    added default values and fixed bugs

--- peonbot.py
+++ peonbot.py
@@ -13,21 +13,33 @@ class PeonBot():
     noStrings = ["I can't do that.", "You stupid?", "Not right..."]
     pissedStrings = ['Whaaat?', 'Me busy. Leave me alone!', 'No time for play.', 'Me not that kind of orc!', 'Stop poking me! Well, that was okay.']
 
-    options = ["start", "poke", "wow", "image", "gif", "youtube", "dice"]
+    options = ["start", "poke", "wow", "image", "gif", "youtube", "roll"]
     jokeStrings = ["I come from the Orcs. We eat with spoons and forks. We love to eat our pork!",
              "It's not easy being green.",
              "Orc smash!",
              "Man, dawg, you know, it's like I'm feeling you, but I'm not feeling you, you know?",
              "I will CRUSH and DESTROY and... ooo... shiny..."]
 
-    helpMessage = """ Something need doing, chieftan? Just say what you want and I be ready to do!
+    youtubeRandomVideos = ["Dog getting a head massage relaxing",
+                           "Nyess",
+                           "HOW TO SAY HELLO IN 30 LANGUAGES",
+                           "Nyan Cat",
+                           "He Man - What's Going On",
+                           "Taking the Hobbits to Isengard",
+                           "10 hours of What is love (Jim Carrey, v.2)",
+                           "Rick Astley - Never Gonna Give You Up",
+                           "Supa hot fire VS b-bone (first battle ORIGINAL)",
+                           "Dramatic Chipmunk",
+                           "What Does The Fox Say?"]
 
-    Here is what I can do:
+    helpMessage = """ Hello, chieftain, I'm your personal peon. I can do some stuff for you, and I be still learning more things.
+
+    Here is what I can do now:
 
     /image   - I can get pretty picture
     /gif     - Hey, this picture moves!
     /youtube - Videos? What be that?
-    /dice    - Wanna play some dice?
+    /roll    - Wanna play some dice?
     /wow     - I hear gossip in the mines sometimes
     /poke    - Stop being annoying
     /joke    - I'm scarry but I can be funny too
@@ -48,6 +60,9 @@ class PeonBot():
                 message = update.message.text
 
                 if message:
+                    if "@peon_bot" in message:
+                        botid = '@peon_bot'
+                        message = message[:message.find(botid)] + message[message.find(botid) + len(botid):]
                     if(message.startswith('/')):
                         command, _, arguments = message.partition(' ')
                         #print('arguments: ' + arguments)
@@ -72,34 +87,43 @@ class PeonBot():
                             self.bot.sendMessage(chat_id=chat_id, text=PeonBot.helpMessage)
 
                         elif command[1:] in PeonBot.options:
-                            if arguments != '':
-                                rand = random.randint(0, len(PeonBot.yesStrings)-1)
-                                self.bot.sendMessage(chat_id=chat_id, text=PeonBot.yesStrings[rand])
-                                if command == '/image':
+                            noArgument = False
+                            if arguments == '':
+                                noArgument = True
+
+                            rand = random.randint(0, len(PeonBot.yesStrings)-1)
+                            self.bot.sendMessage(chat_id=chat_id, text=PeonBot.yesStrings[rand])
+                            if command == '/image':
+                                if noArgument:
+                                    url = self.getImage(None)
+                                else:
                                     url = self.getImage(arguments)
-                                    if url != '':
-                                        self.bot.sendPhoto(chat_id=chat_id, photo=url)
-                                    else:
-                                        self.bot.sendMessage(chat_id=chat_id, text="Got nothin.")    
+                                if url != '':
+                                    self.bot.sendPhoto(chat_id=chat_id, photo=url)
+                                else:
+                                    self.bot.sendMessage(chat_id=chat_id, text="Got nothin.")    
 
-                                elif command == '/gif':
-                                    url = self.getGif(arguments)
-                                    if url != '':
-                                        self.bot.sendPhoto(chat_id=chat_id, photo=url)
-                                    else:
-                                        self.bot.sendMessage(chat_id=chat_id, text="Got nothin.")    
+                            elif command == '/gif':
+                                if noArgument:
+                                    url = self.getImage(None)
+                                else:
+                                    url = self.getImage(arguments)
+                                if url != '':
+                                    self.bot.sendPhoto(chat_id=chat_id, photo=url)
+                                else:
+                                    self.bot.sendMessage(chat_id=chat_id, text="Got nothin.")    
 
-                                elif command == '/youtube':
-                                    videoName, videoURL = self.getYoutube(arguments)
-                                    self.bot.sendMessage(chat_id=chat_id, text=videoName + ' ' + videoURL)
+                            elif command == '/youtube':
+                                videoName, videoURL = self.getYoutube(arguments)
+                                self.bot.sendMessage(chat_id=chat_id, text=videoName + ' ' + videoURL)
 
-                                elif command == '/dice':
+                            elif command == '/roll':
+                                if noArgument:
+                                    roll = self.rollDice('1d6')
+                                    self.bot.sendMessage(chat_id=chat_id, text="Roll 1d6: " + str(roll))
+                                else:
                                     roll = self.rollDice(arguments)
                                     self.bot.sendMessage(chat_id=chat_id, text="Roll " + arguments + ": " + str(roll))
-
-                            else:
-                                rand = random.randint(0, len(PeonBot.noStrings)-1)
-                                self.bot.sendMessage(chat_id=chat_id, text=PeonBot.noStrings[rand] + " (You need to give some information.)")
 
                         else:
                             rand = random.randint(0, len(PeonBot.whatStrings)-1)
@@ -118,17 +142,24 @@ class PeonBot():
         return imgur.get(query, gif=True)
 
     def getYoutube(self, query):
+        if query == "":
+            rand = random.randint(0, len(PeonBot.youtubeRandomVideos)-1)
+            query = PeonBot.youtubeRandomVideos[rand]
+
         result = youtube.search(query)
         name = result['snippet']['title']
         url = "https://youtu.be/" + result['id']['videoId']
         return name, url
 
     def rollDice(self, dices):
+
+        roll = None
         roll = dice.roll(dices)
         diceSum = 0
 
-        for element in roll:
-            diceSum += element
+        if roll != None:
+            for element in roll:
+                diceSum += element
 
         return diceSum
 
